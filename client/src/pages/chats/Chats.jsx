22193f8ff86a5dcf22ee65a85ed9b08a8f004d53
shubhat33 <rashidshubhat41@gmail.com>
2023-09-12 21:50:38 +0530

    chats page frontend completed

--- client/src/pages/chats/Chats.jsx
+++ client/src/pages/chats/Chats.jsx
@@ -2,11 +2,14 @@ import React ,{useState,useEffect}from 'react'
 import Button from '../../components/Button';
 import GroupIcon from '../../components/GroupIcon';
 import Chat from '../../components/Chat';
+const gradient = "bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%"
 const Chats = () => {
 
   // STATE VARIABLES
   const [searchInput, setSearchInput] = useState("");
   const [usersDiv,setUsersDiv] = useState(true)
+  const [seeNav,setSeeNav] = useState(false)
+  const [seeChat,setSeeChat] = useState(true)
 
   const [data, setData] = useState([]);
 
@@ -20,31 +23,38 @@ const Chats = () => {
   }, []);
 
   // FUNCTIONS FOR BUTTONS
-  const homeFun = () =>{}
+  const homeFun = () =>{
+    setSeeChat(true)
+    setUsersDiv(true)
+  }
   const starFun = () =>{}
   const profileFun = () =>{}
   const settingFun = () =>{}
   const logoutFun = () => {}
   const searchFun = () => {}
   const handleSearch = () => {}
+  const chatFun = () => {
+    setSeeChat(true)
+    setUsersDiv(false)}
 
   return (
-    <div className='flex flex-row justify-center min-h-screen'>
+    <div className='chatsPage flex flex-row justify-center h-screen'>
 
         {/* ICON BUTTONS LEFT BAR FOR QUICK ACCESS */}
-        <div className='flex flex-col justify-between items-center bg-[#116D6E] my-[2%] mx-[2%] rounded-lg ml-[2%] px-[1%] min-w-[60px] py-[2%]'>
-
-            <div>
-              <Button src="https://cdn.lordicon.com/qjezzrrz.json" size='40px' clickFun={() => {setUsersDiv(!usersDiv)}}/>
+        <div className={`${seeNav?'flex':'hidden'} sm:flex flex-col justify-between items-center bg-[#116D6E] my-[2%] mx-[2%] rounded-lg ml-[2%] px-[1%] w-[60px] py-[2%]`}>
+            <div className='w-[50px]'>
+              <img src='https://www.pngmart.com/files/16/Speech-Chat-Icon-Transparent-PNG.png' alt='brand' className=']'/>
+              <p className={`font-serif text-xs ${gradient} bg-clip-text  text-transparent`}>Chat App</p>
             </div>
-
             {/* UPPER ICON BUTTONS */}
             <div className='flex flex-col'>
               <Button src="https://cdn.lordicon.com/kxoxiwrf.json" size= '45px' clickFun={homeFun}/>
+              <Button src="https://cdn.lordicon.com/mjmrmyzg.json" size = '45px' clickFun={chatFun} />
               <Button src="https://cdn.lordicon.com/whttoese.json" size= '45px' clickFun={starFun}/>
               <Button src="https://cdn.lordicon.com/ljvjsnvh.json" size= '45px' clickFun={profileFun}/>
               <Button src="https://cdn.lordicon.com/tsnvgrkp.json" size= '45px' clickFun={settingFun}/>
               <Button src="https://cdn.lordicon.com/zniqnylq.json" size= '45px' clickFun={searchFun} />
+             
             </div>
 
             {/* LOGOUT BUTTON ICON */}
@@ -54,7 +64,7 @@ const Chats = () => {
         </div>
 
         {/* ALL CHATS DIV */}
-        <div className={`${usersDiv?"flex":"hidden"} flex-col min-w-[25%] my-[2%] rounded-lg bg-green-50`}>
+        <div className={`${usersDiv?"flex":"hidden"} flex-col my-[2%] rounded-lg bg-green-50 w-[550px]`}>
               {/* USER PROFILE */}
               <div className='flex justify-start'>
                   <div className='w-[15%] h-[15%] my-[2%] ml-[2%]'>
@@ -63,6 +73,10 @@ const Chats = () => {
                   <div className='flex items-center ml-[3%]'>
                     <h2 className='font-serif text-sm font-bold'>User Name</h2>
                   </div>
+                  <div className='flex md:hidden flex items-center justify-end w-[60%]'>
+                    <Button   src="https://cdn.lordicon.com/qjezzrrz.json" clickFun={() => {setSeeNav(!seeNav)}} size='45px' />
+                  </div>
+                
               </div>
 
               {/* GROUP CHATS */}
@@ -101,12 +115,15 @@ const Chats = () => {
         </div>
 
         {/* CURRENT CHAT DIV */}
-        <div className={`${usersDiv?"hidden":"flex"} md:flex  flex-col justify-between min-w-[60%] bg-green-50 my-[2%] mx-[1%] rounded-lg ml-[2%] px-[1%] pt-[1%]`}>
+        <div className={`${usersDiv?"hidden":"flex"} md:${seeChat?'flex':'hidden'}  flex-col justify-between min-w-[50%] w-screen  bg-green-50 my-[2%] mx-[1%] rounded-lg ml-[2%] px-[1%] pt-[1%]`}>
 
             {/* SEARCH IN CHAT */}
-            <div className='w-[100%] flex bg-white px-[2%] rounded-lg'>
+            <div className='w-[100%] flex items-center bg-white px-[2%] rounded-lg'>
               <Button src='https://cdn.lordicon.com/zniqnylq.json' size='25px' />
               <input  onChange={handleSearch} placeholder='Search in chat...' type='search' className='w-[100%] px-[2%] rounded-lg bg-transparent'/>
+              <div className='flex md:hidden flex items-center justify-end w-[60%]'>
+                <Button   src="https://cdn.lordicon.com/qjezzrrz.json" clickFun={() => {setSeeNav(!seeNav)}} size='45px' />
+              </div>
             </div>
 
             <div className='border min-h-[80%] mt-[2%] rounded-lg bg-white '>
@@ -157,15 +174,15 @@ const Chats = () => {
 
             {/* REPLY */}
             <div className='flex py-[1%] justify-around items-center'>
-              <div className='flex pt-[5px] space-x-1'>
-                <Button  src="https://cdn.lordicon.com/fxylrfia.json" size= '25px' clickFun={searchFun} />
-                <Button   src="https://cdn.lordicon.com/brtridhw.json" size= '25px' clickFun={searchFun} />
+              <div className='flex pt-[5px] space-x-2'>
+                <Button  src="https://cdn.lordicon.com/fxylrfia.json" size= '30px' clickFun={searchFun} />
+                <Button   src="https://cdn.lordicon.com/brtridhw.json" size= '30px' clickFun={searchFun} />
               </div>
               
               <p className='text-2xl'>|</p>
-              <input  onChange={handleSearch} placeholder='Message here...' type='search' className='w-[60%] px-[2%] rounded-lg bg-transparent'/>
+              <input  onChange={handleSearch} placeholder='Message here...' type='search' className='h-[40px] w-[60%] px-[2%] rounded-lg bg-transparent'/>
               <button className='bg-sky-500 h-[40px] px-[3%] mx-[1%] rounded-lg font-serif text-white'>
-                Send
+               <p>Send</p>
               </button>
             </div>
 
